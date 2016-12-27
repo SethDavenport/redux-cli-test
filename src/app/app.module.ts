cd1af27bb845ff73d6b4d9710c81c11a45aa6a47
Seth Davenport <seth@rangle.io>
2016-12-27 13:28:22 -0500

    add ng2-redux to repro ngModule bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { NgReduxModule } from 'ng2-redux';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +12,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgReduxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
